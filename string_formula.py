2b79e126dd491a83434accdbe4d3c5aead74a0af
SnipsMine <micha@snippe-mail.nl>
2019-11-03 23:54:10 +0100

    Added the conditianal statement support

--- string_formula.py
+++ string_formula.py
@@ -3,7 +3,7 @@ import re
 import math
 
 
-class StringFormulaReader(object):
+class StringFormulaReader:
     """This class will read a string with a formula and executes it"""
 
     def __init__(self, formula):
@@ -25,8 +25,8 @@ class StringFormulaReader(object):
 
     def create_formula(self):
 
-        # test area https://regex101.com/r/QYbGlh/1
-        regex = re.compile(r"(log\d*)|([a-zA-Z]+)|(\d+)|([+\-*/^])|(.)")
+        # test area https://regex101.com/r/QYbGlh/2
+        regex = re.compile(r"(log\d*)|(if)|([a-z]+)|(\d+)|([+\-*/^])|(.)")
 
         matches = re.finditer(regex, self.string_formula)
 
@@ -36,25 +36,29 @@ class StringFormulaReader(object):
                 value = match.group(1)
                 self.formula.append(value)
 
-            # if the match is a variable
             elif match.group(2) is not None:
                 value = match.group(2)
+                self.formula.append(value)
+
+            # if the match is a variable
+            elif match.group(3) is not None:
+                value = match.group(3)
                 self.formula.append(value)
                 self.variables[value] = None
 
             # if the match is a number
-            elif match.group(3) is not None:
-                value = match.group(3)
+            elif match.group(4) is not None:
+                value = match.group(4)
                 self.formula.append(float(value))
 
             # if the match is a action
-            elif match.group(4) is not None:
-                value = match.group(4)
+            elif match.group(5) is not None:
+                value = match.group(5)
                 self.formula.append(self.actions[value])
 
             # if the match is anything else
             else:
-                value = match.group(5)
+                value = match.group(6)
                 self.formula.append(value)
 
     def link_brackets(self):
@@ -83,8 +87,8 @@ class StringFormulaReader(object):
         if formula == ():
             formula = self.formula
 
-        calculation_rules = ["∑", r'\(\d+', r'log\d*', self.power, "√", self.multiply, self.divide,
-                             self.add, self.subtract]
+        calculation_rules = [r"\{\d+", r'\(\d+', r'log\d*', self.power, "√", self.multiply,
+                             self.divide, self.add, self.subtract]
 
         if None not in self.variables.values():
             for method in calculation_rules:
@@ -94,8 +98,12 @@ class StringFormulaReader(object):
                             if re.search(calculation_rules[1], str(action)):
                                 self.brackets(formula, index)
 
-                            elif action == '∑':
-                                self.sum(formula, index)
+                            elif re.search(calculation_rules[0], str(action)):
+                                if formula[index-1] == "∑":
+
+                                    self.sum(formula, index)
+                                elif formula[index-1] == "if":
+                                    self.conditional(formula, index)
 
                             elif action == "√":
                                 self.square_root(index)
@@ -131,6 +139,8 @@ class StringFormulaReader(object):
             print("there is a variable that has no value")
 
         if len(formula) == 1:
+            if formula[0] in self.variables:
+                formula[0] = self.variables[formula[0]]
             self.answer = formula[0]
 
     def multiply(self, value1, value2):
@@ -210,8 +220,8 @@ class StringFormulaReader(object):
     def sum(self, formula, index):
         print("sum")
 
-        print("".join(formula[index:index+2]))
-        bracket_number = int(formula[index+1].replace("{", ""))
+        print("".join(formula[index]))
+        bracket_number = int(formula[index].replace("{", ""))
 
         start = formula.index("{%s" % str(bracket_number))
         end = formula.index("}%s" % str(bracket_number))
@@ -222,7 +232,7 @@ class StringFormulaReader(object):
 
         answers = []
 
-        if "∑" in s_formula:
+        if re.search(r"{\d+", str(s_formula)):
             start = formula.index("{%s" % str(bracket_number+1))
             end = formula.index("}%s" % str(bracket_number+1))
             s_formula = formula[start-1:end+1]
@@ -254,6 +264,80 @@ class StringFormulaReader(object):
 
         formula.insert(start-1, sum(answers))
 
+    def conditional(self, formula, index):
+        print("conditional")
+
+        logic = {"<": self.smaller, "<=": self.smaller_or_equal, "=": self.equal,
+                 ">=": self.larger_or_equal, ">": self.larger, "!=": self.not_equal}
+
+        bracket_number = int(formula[index].replace("{", ""))
+        start = formula.index("{%s" % str(bracket_number))
+        end = formula.index("}%s" % str(bracket_number))
+        c_formula = formula[start+1:end]
+
+        print(c_formula)
+
+        if re.search(r"{\d+", str(c_formula)):
+            start = formula.index("{%s" % str(bracket_number+1))
+            end = formula.index("}%s" % str(bracket_number+1))
+            c_formula = formula[start-1:end+1]
+            self.execute(c_formula)
+        else:
+            counter = 0
+            form = [[], [], [], []]
+            logical = ""
+            for x in c_formula:
+                if x == ";":
+                    counter += 1
+                    continue
+
+                if x in logic.keys():
+                    counter += 1
+                    logical = x
+                    continue
+
+                form[counter].append(x)
+
+            for f in form[0:2]:
+                self.execute(f)
+
+            print(form)
+
+            value1 = form[0][0]
+            value2 = form[1][0]
+
+            logic[logical](value1, value2)
+
+            if self.answer:
+                self.execute(form[2])
+            else:
+                self.execute(form[3])
+
+            print(self.answer)
+
+        for rm in range(start-1, end+1):
+            formula.pop(start-1)
+
+        formula.insert(start-1, self.answer)
+
+    def smaller(self, value1, value2):
+        self.answer = value1 < value2
+
+    def smaller_or_equal(self, value1, value2):
+        self.answer = value1 <= value2
+
+    def equal(self, value1, value2):
+        self.answer = value1 == value2
+
+    def larger_or_equal(self, value1, value2):
+        self.answer = value1 >= value2
+
+    def larger(self, value1, value2):
+        self.answer = value1 > value2
+
+    def not_equal(self, value1, value2):
+        self.answer = value1 != value2
+
 
 def main():
     formula1 = StringFormulaReader("x*3/5+20+2^(z*((5+3)*3))")
@@ -275,10 +359,16 @@ def main():
     logarithm.variables["x"] = 100
     logarithm.execute()
 
+    conditional = StringFormulaReader("if{x<if{x>y*2;x;y};x;y}")
+    conditional.variables["x"] = 10
+    conditional.variables["y"] = 11
+    conditional.execute()
+
     print("standard deviation =", round(standard_deviation.answer, 4))
     print("Mean =", round(mean.answer, 4))
     print("Formula 1 =", round(formula1.answer, 4))
     print("Logarithm = ", round(logarithm.answer, 4))
+    print("Conditional = ", conditional.answer)
 
 
 if __name__ == "__main__":
